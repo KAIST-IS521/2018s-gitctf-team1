8dbbddbfe3f102f79a54bc85fbcb7be46f3b3000
Seong Ho Han <hansh09@kaist.ac.kr>
2018-05-19 06:13:40 +0900

    clean up DataHandler.cpp & DataHandler.h + add TODO

--- src/DataHandler.cpp
+++ src/DataHandler.cpp
@@ -4,19 +4,19 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-DataHandler::DataHandler(string client) {
-}
+using namespace std;
 
-DataHandler::~DataHandler() {
-}
+DataHandler::DataHandler(std::string client) {}
+
+DataHandler::~DataHandler() {}
 
 bool DataHandler::verify_path(std::string path) {
     bool path_ok = false;
 
     // verify that the path is not all slashes
     // there's a very nice uncaught exception when we pass '//+' to open()
-    for(char c : path) {
-        if (c != '/') {
+    for(char c : path) {      // is it really works?
+        if (c != '/') {  
             path_ok = true;
             break;
         }
@@ -25,7 +25,7 @@ bool DataHandler::verify_path(std::string path) {
     return path_ok;
 }
 
-bool is(char * mime, std::string type) {
+bool is(char *mime, std::string type) {
     return (strstr(mime, type.c_str()) != NULL);
 }
 
@@ -35,11 +35,12 @@ DataHandler::resource DataHandler::read_resource(std::string path) {
 DataHandler::resource DataHandler::read_resource(std::string path, std::string cookies) {
     return DataHandler::read_resource(path, cookies, NULL);
 }
-DataHandler::resource DataHandler::read_resource(std::string path, std::string cookies, DataHandler::resource * data) {
+DataHandler::resource DataHandler::read_resource(std::string path, std::string cookies, DataHandler::resource *data) {
     // prepend cwd() to path
 
-    if (!verify_path(path))
+    if (!verify_path(path)){
         path = "/index.html";
+	}
 
     std::string cwd = get_working_path();
     path = cwd + path;
@@ -47,7 +48,7 @@ DataHandler::resource DataHandler::read_resource(std::string path, std::string c
     // first check if such a file even exists
     int fd = open(path.c_str(), O_RDONLY);
     if (fd < 0) {
-        char * err = std::strerror(errno);
+        char *err = std::strerror(errno);
         throw DataHandler::FileNotFound("Error while reading file contents at " + path + ": " + std::string(err ? err : "unknown error"));
     }
     close(fd);
@@ -56,10 +57,10 @@ DataHandler::resource DataHandler::read_resource(std::string path, std::string c
     DataHandler::Exec runner;
     std::string args[2] = { "/usr/bin/file", path };
     DataHandler::resource file_mime = runner.run_command(args);
-    char * mime = file_mime.data;
-    std::string ext = path.substr(path.length()-3);
+    char *mime = file_mime.data;
+    /* std::string ext = path.substr(path.length()-3);
     for(char c : ext)
-        c = std::toupper(c);
+        c = std::toupper(c); */ 
 
     DataHandler::resource output;
     // now check for known mime types
@@ -75,9 +76,9 @@ DataHandler::resource DataHandler::read_resource(std::string path, std::string c
         std::string error_str = "Unsupported mime type: " + std::string(mime);
         // drop 'local' part of path
         size_t pos = 0;
-        while ((pos = error_str.find(cwd)) != std::string::npos )
+        while ((pos = error_str.find(cwd)) != std::string::npos)
             error_str.erase(pos, cwd.length());
-
+		// TODO: fix it!
         throw DataHandler::Unsupported(error_str);
     }
 
@@ -85,8 +86,8 @@ DataHandler::resource DataHandler::read_resource(std::string path, std::string c
 }
 
 std::string DataHandler::get_working_path() {
-   char temp[PATH_MAX];
-   return ( getcwd(temp, PATH_MAX) ? std::string( temp ) : std::string("") );
+   char temp[PATH_MAX]; // TODO: define PATH_MAX
+   return ( getcwd(temp, PATH_MAX) ? std::string(temp) : std::string("") );
 }
 
 DataHandler::resource DataHandler::get_error_file(int error_code, std::string param) {
@@ -94,7 +95,7 @@ DataHandler::resource DataHandler::get_error_file(int error_code, std::string pa
     DataHandler::resource output = DataHandler::read_resource("/errors/"+ std::to_string(error_code) +".html");
     // now prepare a place to write the filled template
     long new_size = output.size + param.length() - 3;
-    char * data = (char *) malloc((new_size+1) * sizeof(char));
+    char *data = (char *) malloc((new_size+1) * sizeof(char));
     // and fill it
     sprintf(data, output.data, param.c_str());
 
--- src/DataHandler.h
+++ src/DataHandler.h
@@ -12,60 +12,60 @@
 using namespace std;
 
 class DataHandler {
-public:
-    struct resource {
-        string type;
-        char * data;
-        long size;
-    };
+	private:
+    	std::string get_working_path();
+    	bool verify_path(std::string path);
 
-private:
-    std::string get_working_path();
-    bool verify_path(std::string path);
-public:
-    DataHandler(string client);
-    virtual ~DataHandler();
-    resource read_resource(std::string path);
-    resource read_resource(std::string path, std::string cookies);
-    resource read_resource(std::string path, std::string cookies, DataHandler::resource * data);
-    resource get_error_file(int error_code, std::string param);
+	public:
+		struct resource {
+			string type;
+			char *data;
+			long size;
+		}
 
-    // Exception base
-    class Exception: public BaseException {
-        public:
-            Exception(std::string msg = "Unknown data handler exception") {
+    	DataHandler(std::string client);
+    	virtual ~DataHandler();
+    	resource read_resource(std::string path);
+    	resource read_resource(std::string path, std::string cookies);
+    	resource read_resource(std::string path, std::string cookies, DataHandler::resource *data);
+    	resource get_error_file(int error_code, std::string param);
+
+    	// Exception base
+    	class Exception: public BaseException {
+        	public:
+            	Exception(std::string msg = "Unknown data handler exception") {
                 this->reason = msg;
             }
-    };
+    	};
 
-    class Exec {
-        public:
-            Exec();
-            ~Exec();
-            resource run_command(std::string args[]);
-            resource run_command(std::string args[], DataHandler::resource * data);
-            resource run_command(std::string args[], DataHandler::resource * data, std::string cookies);
+  		class Exec {
+        	public:
+            	Exec();
+            	~Exec();
+            	resource run_command(std::string args[]);
+            	resource run_command(std::string args[], DataHandler::resource *data);
+				resource run_command(std::string args[], DataHandler::resource *data, std::string cookies);
 
-        class PermissionDenied: public Exception {
-            public:
-                PermissionDenied(std::string msg = "Permission denied while trying to execute command") {
-                    this->reason = msg;
-                }
-        };
-    };
+        		class PermissionDenied: public Exception {
+            		public:
+                		PermissionDenied(std::string msg = "Permission denied while trying to execute command") {
+                    	this->reason = msg;
+                		}
+        		};
+    	};
 
-    class FileNotFound: public Exception {
-        public:
-            FileNotFound(std::string msg = "Could not find requested file") {
+    	class FileNotFound: public Exception {
+        	public:
+            	FileNotFound(std::string msg = "Could not find requested file") {
                 this->reason = msg;
-            }
-    };
-    class Unsupported: public Exception {
-        public:
-            Unsupported(std::string msg = "Unsupported file found") {
-                this->reason = msg;
-            }
-    };
+       }
+    	};
+    	class Unsupported: public Exception {
+        	public:
+            	Unsupported(std::string msg = "Unsupported file found") {
+                	this->reason = msg;
+            	}
+    	};
 };
 
 #endif /* SRC_DATAHANDLER_H_ */
