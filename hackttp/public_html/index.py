93370fca4d72e9b0e4d2e34ce22cb1a55d01987b
Park Jung Hwan <ahnmo@pagez.kr>
2018-05-20 02:53:19 +0900

    Add test public_html

---
+++ hackttp/public_html/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import sys
+
+data = sys.stdin.readline()
+
+sys.stdout.write("HTTP/1.1 200 OK\r\n")
+#sys.stdout.write("Date: Mon, 27 Jul 2009 12:28:53 GMT\r\n"
+sys.stdout.write("Server: Apache/2.2.14 (Win32)\r\n")
+#sys.stdout.write("Last-Modified: Wed, 22 Jul 2009 19:15:56 GMT\r\n"
+sys.stdout.write("Content-Length: %d\r\n" % len(data))
+sys.stdout.write("Content-Type: text/html\r\n")
+sys.stdout.write("Connection: Close\r\n")
+sys.stdout.write("\r\n")
+sys.stdout.write(data)
