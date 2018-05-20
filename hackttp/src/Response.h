b1a62a99ecbce580192d81f2957af7939f63ef23
Park Jung Hwan <ahnmo@pagez.kr>
2018-05-21 01:24:30 +0900

    Fix status code 302

--- hackttp/src/Response.h
+++ hackttp/src/Response.h
@@ -6,6 +6,7 @@
 
 // HTTP STATUS CODES
 #define HTTP_OK                 200
+#define HTTP_FOUND              302
 #define HTTP_BAD_REQUEST        400
 #define HTTP_FORBIDDEN          403
 #define HTTP_NOT_FOUND          404
@@ -16,15 +17,15 @@
 //class DataHandler;
 
 class Response {
-    public:
-        Response(int code, DataHandler::Resource rsrc);
-        Response();
-        std::string render();
-        static Response *fromCode(int code);
-    private:
-        int status_code;
-        std::string status;
-        int length;
-        std::map<std::string, std::string> headers;
-        std::string body;
+public:
+  Response(int code, DataHandler::Resource rsrc);
+  Response();
+  std::string render();
+  static Response *fromCode(int code);
+private:
+  int status_code;
+  std::string status;
+  int length;
+  std::map<std::string, std::string> headers;
+  std::string body;
 };
